0d0fb38329bc961739f7d1a75ba56fc2583efdbc
minuetchoi-music <minuetchoi@naver.com>
2022-04-02 12:21:27 +0900

    test

--- public/js/custom/custom.js
+++ public/js/custom/custom.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    // 영상 strt
     var $video = $('video');
 
     if ($video.length > 0) {
@@ -10,7 +11,7 @@ $(document).ready(function(){
             cache: false,
             datatype: "JSON",
             success: function (arr) {
-               var player = videojs('player', {
+                var player = videojs('player', {
                     autoplay: true,
                     loop: true,
                     muted: true,
@@ -22,26 +23,50 @@ $(document).ready(function(){
                 player.playlist.repeat(true);
             },
             error: function (xhr, status, error) {
-                console.log("ERROR!!!"); 
+                console.log("ERROR!!!");
             }
         });
     }
+    // --영상 end
+
+
+    // 북마크 strt
+    $('.bookmark').magnificPopup({
+        type:'iframe',
+        iframe: {
+            markup: '<style>.mfp-iframe-holder .mfp-content {max-width: 100%;height:100%}</style>'+
+                    '<div class="mfp-iframe-scaler" >'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '</div></div>'
+        }
+    });
+    // --북마크 end
 });
 
+// 영상 셔플
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
+    let currentIndex = array.length, randomIndex;
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
+}
+
+// 북마크
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
