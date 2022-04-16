8b4c3d85627f8ee25920784721eaa5daba1ce9eb
minuetchoi-music <minuetchoi@naver.com>
2022-04-16 21:20:43 +0900

    test

--- public/js/custom/custom.js
+++ public/js/custom/custom.js
@@ -67,7 +67,7 @@ function shuffle(array) {
 
 // 북마크
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
